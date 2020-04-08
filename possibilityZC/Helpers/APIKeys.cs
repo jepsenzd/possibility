8094229e75d7f69286ed8d9c7a4ba47ea4dff3fe
jepsenzd <45468749+jepsenzd@users.noreply.github.com>
2020-04-08 02:23:23 -0400

    Update APIKeys.cs

--- possibilityZC/Helpers/APIKeys.cs
+++ possibilityZC/Helpers/APIKeys.cs
@@ -10,10 +10,10 @@ namespace possibilityZC.Helpers
         {
         }
 
-        public static readonly string CosmosEndpointUrl = "https://possibilitycosmosdb.documents.azure.com:443/";
+        public static readonly string CosmosEndpointUrl = "COSMOS URL HERE";
 
-        public static readonly string CosmosAuthKey = "YQ5xXwSqbRUfD6tT7JcS6AvHrLsZDM7VYFFXZpXZKMQIVJ0ygWuPKdM040cbnbLKTMiLD5q9MXZatiav0plECw==";
+        public static readonly string CosmosAuthKey = "COSMOS AUTH KEY HERE";
 
-        public const string GoogleMapsApiKey = "AIzaSyA6j8-kpdoXM6iCDvNV8JDoPOWBFvRDPIg";
+        public const string GoogleMapsApiKey = "GOOGLE MAPS API KEY HERE";
     }
 }
